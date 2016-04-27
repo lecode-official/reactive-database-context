7cf8699d6e8ee1a4a8ab638a7985207f58d54148
David Neumann <david.neumann@lecode.de>
2016-04-27 15:06:30 +0200

    Created the solution and cleaned it up.

---
+++ System.Data.Entity.Reactive/Properties/AssemblyInfo.cs
@@ -0,0 +1,27 @@
+﻿
+#region Using Directives
+
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+#endregion
+
+#region General Assembly Information
+
+[assembly: AssemblyTitle("Reactive Database Context")]
+[assembly: AssemblyDescription("A simple and light-weight wrapper around Entity Framework's DbContext, which makes it possible to react to changes using the Reactive Extensions.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Reactive Database Context")]
+[assembly: AssemblyCopyright("Copyright © David Neumann & Lukas Rögner 2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("e4b7c5f6-5b7b-479b-92f7-294302fb7fa8")]
+
+#region Assembly Version Information
+
+[assembly: AssemblyVersion("0.0.1.*")]
+[assembly: AssemblyFileVersion("0.0.1.0")]
+
+#endregion
